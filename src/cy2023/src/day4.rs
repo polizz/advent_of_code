5b876ee8bf9f324e84ebc91e453bab0b1fce9013
polizz <polizz@nowhere>
2023-12-10 00:55:40 -0600

    Day 4 part 2

--- src/cy2023/src/day4.rs
+++ src/cy2023/src/day4.rs
@@ -52,12 +52,98 @@ mod tests {
     use std::fs::File;
     use std::io::prelude::*;
 
+    #[derive(Debug)]
+    struct CountData {
+        card_score: usize,
+        copies: usize,
+    }
+
     fn load_file() -> String {
         let mut file = File::open("src/fixtures/day4.txt").unwrap();
         let mut file_contents = String::new();
         let _ = file.read_to_string(&mut file_contents);
 
         file_contents
+    }
+
+    #[test]
+    fn day4_test2() {
+        let start = std::time::Instant::now();
+
+        let input = r#"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#;
+
+        let card_stack = parse_stack.parse_next(&mut input.as_ref()).unwrap();
+
+        let mut waterfall_count: Vec<CountData> = card_stack
+            .iter()
+            .map(|(winners, ours)| {
+                let card_score = ours.iter().filter(|o| winners.contains(&o)).count();
+
+                CountData {
+                    card_score,
+                    copies: 1,
+                }
+            })
+            .collect();
+
+        for i in 0..waterfall_count.len() {
+            let CountData { copies, card_score } = waterfall_count[i];
+
+            for _ in 0..copies {
+                for ix in 1..=card_score {
+                    if i + ix < card_stack.len() {
+                        waterfall_count[i + ix].copies += 1;
+                    }
+                }
+            }
+        }
+
+        let sum: usize = waterfall_count.iter().map(|c| c.copies).sum();
+        assert_eq!(sum, 30usize);
+
+        println!("Process in: {:?}", start.elapsed());
+    }
+
+    #[test]
+    fn day4_file2() {
+        let start = std::time::Instant::now();
+        let input = load_file();
+
+        let card_stack = parse_stack.parse_next(&mut input.as_ref()).unwrap();
+
+        let mut waterfall_count: Vec<CountData> = card_stack
+            .iter()
+            .map(|(winners, ours)| {
+                let card_score = ours.iter().filter(|o| winners.contains(&o)).count();
+
+                CountData {
+                    card_score,
+                    copies: 1,
+                }
+            })
+            .collect();
+
+        for i in 0..waterfall_count.len() {
+            let CountData { copies, card_score } = waterfall_count[i];
+
+            for _ in 0..copies {
+                for ix in 1..=card_score {
+                    if i + ix < card_stack.len() {
+                        waterfall_count[i + ix].copies += 1;
+                    }
+                }
+            }
+        }
+
+        let sum: usize = waterfall_count.iter().map(|c| c.copies).sum();
+        assert_eq!(sum, 14624680usize);
+
+        println!("Process in: {:?}", start.elapsed());
     }
 
     #[test]
